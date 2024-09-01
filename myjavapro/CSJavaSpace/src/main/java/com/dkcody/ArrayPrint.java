2c86030ecaaa7b49601d7973270015c89d8ae090
Dinesh Kumar A <dineshkumar.pro2k@gmail.com>
2024-09-01 15:29:31 +0000

    Third commit

--- myjavapro/CSJavaSpace/src/main/java/com/dkcody/ArrayPrint.java
+++ myjavapro/CSJavaSpace/src/main/java/com/dkcody/ArrayPrint.java
@@ -1,12 +1,22 @@
 package main.java.com.dkcody;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class ArrayPrint {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        System.out.println(a1+""+a2);
         ArrtoStr(new int[]{1,2,3,4,5,9});
     }
     public static void ArrtoStr(int[] arr){
         System.out.println(Arrays.toString(arr));
+        
+        
+        
     }
 
 }
