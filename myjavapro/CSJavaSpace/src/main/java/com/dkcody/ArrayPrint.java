d69be793a834fe10064db5924c3f4a58bc3eda4c
Dinesh Kumar A <dineshkumar.pro2k@gmail.com>
2024-09-01 14:51:41 +0000

    Second commit

---
+++ myjavapro/CSJavaSpace/src/main/java/com/dkcody/ArrayPrint.java
@@ -0,0 +1,12 @@
+package main.java.com.dkcody;
+import java.util.*;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        ArrtoStr(new int[]{1,2,3,4,5,9});
+    }
+    public static void ArrtoStr(int[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
--- myjavapro/CSJavaSpace/src/main/java/com/dkcody/HelloWorld.java
+++ myjavapro/CSJavaSpace/src/main/java/com/dkcody/HelloWorld.java
@@ -1,4 +1,4 @@
-package main;
+package main.java.com.dkcody;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
