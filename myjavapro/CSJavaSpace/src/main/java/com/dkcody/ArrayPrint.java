6c4c5d36d99527f266bc2f3ff23b5a2dd2ff0c46
Dinesh Kumar A <dineshkumar.pro2k@gmail.com>
2024-09-17 02:29:44 +0000

    New commit

--- myjavapro/CSJavaSpace/src/main/java/com/dkcody/ArrayPrint.java
+++ myjavapro/CSJavaSpace/src/main/java/com/dkcody/ArrayPrint.java
@@ -1,22 +1,35 @@
 package main.java.com.dkcody;
 
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.stream.*;
 
 
 public class ArrayPrint {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a1 = sc.nextInt();
-        int a2 = sc.nextInt();
-        System.out.println(a1+""+a2);
-        ArrtoStr(new int[]{1,2,3,4,5,9});
-    }
-    public static void ArrtoStr(int[] arr){
-        System.out.println(Arrays.toString(arr));
-        
-        
-        
+        ArrayPrint solution = new ArrayPrint();
+
+        // Test case 
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        solution.merge(nums1, m, nums2, n);
+
+        System.out.println("Merged array: " + Arrays.toString(nums1));  
+        // Output should be [1, 2, 2, 3, 5, 6]
     }
 
-}
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Convert IntStream to Stream<Integer> using boxed()
+        int[] mergedArray = Stream.concat(
+                Arrays.stream(nums1, 0, m).boxed(),  // First m elements of nums1, boxed to Stream<Integer>
+                Arrays.stream(nums2, 0, n).boxed()   // First n elements of nums2, boxed to Stream<Integer>
+        ).sorted().mapToInt(Integer::intValue).toArray();  // Convert Stream<Integer> back to int[]
+        
+        // Copy the sorted merged array back into nums1
+        System.arraycopy(mergedArray, 0, nums1, 0, m + n);
+    }
+ }
+
+
